2ae7237b3eedf0c739f1368bd7039a6c532f525d
Okenna C Okpareke <ookpareke@gmail.com>
2019-09-08 16:10:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,13 +9,14 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5]  # Middle row
-  [6,7,8]  # Bottom row
-  [0,3,6]  # Left column
-  [1,4,7]  # Middle column
-  [2,5,8]  # Right column 
-  [0,4,8]  # Right Diagnol
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left column
+  [1,4,7],  # Middle column
+  [2,5,8],  # Right column 
+  [0,4,8],  # Right Diagnol
   [2,4,6]  # Left Diagnol
+  ]
   ```
  X | X | X
 -----------
